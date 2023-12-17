259bec67b0a348eb3df60bee77eba32f10112f6f
Markus Hällfors <markus@hallfors.com>
2023-12-17 20:09:33 +0200

    Add camera scroll zoom

--- CameraController.gd
+++ CameraController.gd
@@ -4,11 +4,19 @@ var MOVEMENT_SCROLL_SPEED = 0.05;
 var isRightButtonDown: bool = false;
 var oldCursorPosition: Vector2;
 
+var cameraTween
+var cameraZoon
+
+var MAX_ZOOM: int = 30
+var MIN_ZOOM: int = 10
+var ZOOM_INCREMENT: int = 2
+
 var mouseCursor = load("res://icons8-cursor-24.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(mouseCursor)
+	cameraZoon = position.y
 
 
 func _input(event):
@@ -25,7 +33,24 @@ func _input(event):
 	if event is InputEventMouseMotion && isRightButtonDown:
 		position.x += event.relative.x * MOVEMENT_SCROLL_SPEED;
 		position.z += event.relative.y * MOVEMENT_SCROLL_SPEED;
+		
+	# Mouse scroll zoom
+	
+	if event.is_action_pressed("scroll_in"):
+		cameraZoon = max(cameraZoon-ZOOM_INCREMENT, MIN_ZOOM)
+		ZoomTween(cameraZoon)
+	
+	if event.is_action_pressed("scroll_out"):
+		cameraZoon = min(cameraZoon+ZOOM_INCREMENT, MAX_ZOOM)
+		ZoomTween(cameraZoon)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func ZoomTween(zoom: int) -> void:
+	if cameraTween: 
+		cameraTween.kill()
+	
+	cameraTween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	cameraTween.tween_property(self, "position:y", zoom, 1)
