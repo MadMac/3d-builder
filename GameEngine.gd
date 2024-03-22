eb05b1bbe4e8426aa9335e80437e4bbdf0172f9f
Markus Hällfors <markus@hallfors.com>
2024-03-22 22:14:07 +0200

    Start adding unit selector square

--- GameEngine.gd
+++ GameEngine.gd
@@ -6,6 +6,9 @@ const RAY_LENGTH = 1000
 # Tracks which units are selected
 var selectedUnits: Array = []
 var rightMouseButtonTimer: Timer
+var mouse_dragging: bool = false
+var mouse_drag_init: Vector2
+var unitSelector
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,11 +16,17 @@ func _ready():
 	rightMouseButtonTimer.one_shot = true
 	rightMouseButtonTimer.autostart = false
 	rightMouseButtonTimer.wait_time = 0.5	
+	unitSelector = get_node("UnitSelector")
 	add_child(rightMouseButtonTimer)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if mouse_dragging:
+		unitSelector.visible = true
+		var mousepos = get_viewport().get_mouse_position()
+		unitSelector.set_coords(mouse_drag_init, mousepos)
+	else: 
+		unitSelector.visible = false
 
 func _input(event):
 	
@@ -29,9 +38,15 @@ func _input(event):
 	
 	# UNIT SELECTION
 	if event is InputEventMouseButton && event.button_index == 1:
-		if event.pressed:
+		if event.is_pressed():
+			mouse_dragging = true
+			mouse_drag_init = event.position
+			unitSelector.set_coords(mouse_drag_init, mouse_drag_init)
+			
+		if event.is_released():
 			var mouse_pos = get_mouse_to_world(event)
 			print(mouse_pos.collider.name)
+			mouse_dragging = false
 			if mouse_pos.collider.name == "PawnCharacter":
 				if selectedUnits.has(mouse_pos.collider):
 					print("Object already in array")
---
+++ UnitSelect.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+
+var head : PackedVector2Array
+
+var coords_head : Array = [
+	[ 10, 10 ],  [ 10, 50 ],
+	[ 50, 50 ], [ 50, 10 ],
+
+	]
+
+func float_array_to_Vector2Array(coords : Array) -> PackedVector2Array:
+	# Convert the array of floats into a PackedVector2Array.
+	var array : PackedVector2Array = []
+	for coord in coords:
+		array.append(Vector2(coord[0], coord[1]))
+	return array
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _draw():
+	# We are going to paint with this color.
+	var godot_blue : Color = Color("478cbf")
+	# We pass the PackedVector2Array to draw the shape.
+	print(head)
+	draw_polygon(head, [ godot_blue ])
+	
+	
+func set_coords(start: Vector2, end: Vector2):
+	coords_head = [
+		[start.x, start.y],
+		[start.x, end.y],
+		[end.x, end.y],
+		[end.x, start.y]
+	]
+	head = float_array_to_Vector2Array(coords_head);
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	queue_redraw()
